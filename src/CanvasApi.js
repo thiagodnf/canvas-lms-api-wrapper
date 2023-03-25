253516f27dbc5da881c551ac992b2de800c5d630
Thiago Ferreira <thiagodnf@gmail.com>
2023-03-24 21:19:30 -0400

    added some jsdoc

--- src/CanvasApi.js
+++ src/CanvasApi.js
@@ -2,16 +2,30 @@ import Pages from "./resources/Pages.js";
 import Syllabus from "./resources/Syllabus.js";
 import GradingScheme from "./resources/GradingScheme.js";
 
+/**
+ * Class responsible for managing all available APi's
+ *
+ * @author Thiago Ferreira
+ */
 export default class CanvasApi {
 
+    /**
+     * @returns {Pages} Pages's API
+     */
     pages() {
         return new Pages();
     }
 
+    /**
+     * @returns {Syllabus} Syllabus's API
+     */
     syllabus() {
         return new Syllabus();
     }
 
+    /**
+     * @returns {GradingScheme} Grading Scheme's API
+     */
     gradingScheme() {
         return new GradingScheme();
     }
